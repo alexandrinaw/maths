7f46121da8d3c6a73f6485f7bca9490170268c89
Mary Rose Cook <maryrosecook@maryrosecook.com>
2013-06-05 20:06:13 -0400

    Fix weird indentation.

--- math.js
+++ math.js
@@ -17,14 +17,14 @@ var isPartOfMultiCharToken = function(charStr) {
 };
 
 function tokenize(input, inProgressToken) {
-	if (input.length === 0) {
+  if (input.length === 0) {
     return [];
   } else if (isPartOfMultiCharToken(input[0])) {
     inProgressToken = (inProgressToken || "") + input[0];
     if (isPartOfMultiCharToken(input[1])) {
-		  return tokenize(input.slice(1), inProgressToken);
+      return tokenize(input.slice(1), inProgressToken);
     } else {
-		  return [inProgressToken].concat(tokenize(input.slice(1)));
+      return [inProgressToken].concat(tokenize(input.slice(1)));
     }
   } else {
     return [input[0]].concat(tokenize(input.slice(1)));
@@ -32,16 +32,16 @@ function tokenize(input, inProgressToken) {
 }
 //takes an array of numbers/operators and creates a multi-dimensional array when there are parenthesis
 function formatArray(lexedArray) {
-	if (lexedArray.length==0) {
+  if (lexedArray.length==0) {
 		return [];
   } else if (lexedArray[0]==="(") {
-	  var rest = lexedArray.slice(1);
-		var lastIndex = lastIndexOf(")", rest);
-		var element = rest.slice(0, lastIndex); //element = middle chunk between ()
+    var rest = lexedArray.slice(1);
+    var lastIndex = lastIndexOf(")", rest);
+    var element = rest.slice(0, lastIndex); //element = middle chunk between ()
     var restAfterElement = rest.slice(lastIndex+1);
-	  return [formatArray(element)].concat(formatArray(restAfterElement));
-	} else {
-	  return [lexedArray[0]].concat(formatArray(lexedArray.slice(1)));
+    return [formatArray(element)].concat(formatArray(restAfterElement));
+  } else {
+    return [lexedArray[0]].concat(formatArray(lexedArray.slice(1)));
   }
 }
 
@@ -93,13 +93,13 @@ var operatorFns = {
 
 // takes a multi-dimensional array of numbers/operators and performs calculations, returning the result
 function doMath(nestedMath) {
-	//formattedInput example: [["2.1", "*", "17"], "-", "3"]
+  //formattedInput example: [["2.1", "*", "17"], "-", "3"]
   if (nestedMath.length === 0) {
     return;
   }
   var operandA = Array.isArray(nestedMath[0]) ? doMath(nestedMath[0]) : nestedMath[0];
   var operandB = Array.isArray(nestedMath[2]) ? doMath(nestedMath[2]) : nestedMath[2];
-	var operator = nestedMath[1];
+  var operator = nestedMath[1];
   return operatorFns[operator](parseFloat(operandA), parseFloat(operandB));
 }
 
